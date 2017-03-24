82948ad48d3b46e11552b75465442c6c06293f87
Richard Dotchin <richard.dotchin@gmail.com>
2017-03-24 16:56:57 +0000

    TidyGit

--- assets/API/dictionary.js
+++ assets/API/dictionary.js
@@ -4,21 +4,26 @@ var word = "dance";
 
 
 $.ajax({
-    url: "https://wordsapiv1.p.mashape.com/words/last/similarTo", // The URL to the API. You can get this in the API page of the API you intend to consume
-    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-    data: {}, // Additional parameters here
-    dataType: 'json',
-    success: function(data) { console.dir((data.source)); console.log(data);},
-    error: function(err) { alert(err); },
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", "JVsFpSsea5mshtsH7N5dZQOYQd0yp1dqScujsnjdKNIoipqLfS"); // Enter here your Mashape key
-    }
+      url: "https://wordsapiv1.p.mashape.com/words/last/similarTo", // The URL to the API. You can get this in the API page of the API you intend to consume
+      type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+      data: {}, // Additional parameters here
+      dataType: 'json',
+      success: function(data) {
+            console.dir((data.source));
+            console.log(data);
+      },
+      error: function(err) {
+            alert(err);
+      },
+      beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "JVsFpSsea5mshtsH7N5dZQOYQd0yp1dqScujsnjdKNIoipqLfS"); // Enter here your Mashape key
+      }
 
 
-}).done(function(response){
+}).done(function(response) {
 
 
-	console.log(response);
+      console.log(response);
 
 
 });
--- assets/API/youtube.js
+++ assets/API/youtube.js
@@ -1,11 +1,10 @@
-
 // The client ID is obtained from the Google Developers Console
 // at https://console.developers.google.com/.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
 var OAUTH2_CLIENT_ID = '758489283797-q6dhsfjfkodjehg2mnnp08nm6ahgoehf.apps.googleusercontent.com';
 var OAUTH2_SCOPES = [
-  'https://www.googleapis.com/auth/youtube'
+      'https://www.googleapis.com/auth/youtube'
 ];
 
 // I added this
@@ -13,9 +12,9 @@ var OAUTH2_SCOPES = [
 
 // Upon loading, the Google APIs JS client automatically invokes this callback.
 googleApiClientReady = function() {
-  gapi.auth.init(function() {
-    window.setTimeout(checkAuth, 1);
-  });
+      gapi.auth.init(function() {
+            window.setTimeout(checkAuth, 1);
+      });
 }
 
 // Attempt the immediate OAuth 2.0 client flow as soon as the page loads.
@@ -24,39 +23,39 @@ googleApiClientReady = function() {
 // succeeds with no user intervention. Otherwise, it fails and the
 // user interface that prompts for authorization needs to display.
 function checkAuth() {
-  gapi.auth.authorize({
-    client_id: OAUTH2_CLIENT_ID,
-    scope: OAUTH2_SCOPES,
-    immediate: true
-  }, handleAuthResult);
+      gapi.auth.authorize({
+            client_id: OAUTH2_CLIENT_ID,
+            scope: OAUTH2_SCOPES,
+            immediate: true
+      }, handleAuthResult);
 }
 
 // Handle the result of a gapi.auth.authorize() call.
 function handleAuthResult(authResult) {
-  if (authResult && !authResult.error) {
-    // Authorization was successful. Hide authorization prompts and show
-    // content that should be visible after authorization succeeds.
-    $('.pre-auth').hide();
-    $('.post-auth').show();
-    loadAPIClientInterfaces();
-  } else {
-    // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
-    // client flow. The current function is called when that flow completes.
-    $('#login-link').click(function() {
-      gapi.auth.authorize({
-        client_id: OAUTH2_CLIENT_ID,
-        scope: OAUTH2_SCOPES,
-        immediate: false
-        }, handleAuthResult);
-    });
-  }
+      if (authResult && !authResult.error) {
+            // Authorization was successful. Hide authorization prompts and show
+            // content that should be visible after authorization succeeds.
+            $('.pre-auth').hide();
+            $('.post-auth').show();
+            loadAPIClientInterfaces();
+      } else {
+            // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
+            // client flow. The current function is called when that flow completes.
+            $('#login-link').click(function() {
+                  gapi.auth.authorize({
+                        client_id: OAUTH2_CLIENT_ID,
+                        scope: OAUTH2_SCOPES,
+                        immediate: false
+                  }, handleAuthResult);
+            });
+      }
 }
 
 // Load the client interfaces for the YouTube Analytics and Data APIs, which
 // are required to use the Google APIs JS client. More info is available at
 // http://code.google.com/p/google-api-javascript-client/wiki/GettingStarted#Loading_the_Client
 function loadAPIClientInterfaces() {
-  gapi.client.load('youtube', 'v3', function() {
-    handleAPILoaded();
-  });
+      gapi.client.load('youtube', 'v3', function() {
+            handleAPILoaded();
+      });
 }
